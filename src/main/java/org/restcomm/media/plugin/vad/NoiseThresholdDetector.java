c6befdf1becc906fb223e994327a821d6d88d697
Vladimir Morosev <vladimir.morosev@telestax.com>
2018-02-09 17:01:44 +0100

    RMS-8 #comment Migrated speech detector component from core project

---
+++ src/main/java/org/restcomm/media/plugin/vad/NoiseThresholdDetector.java
@@ -0,0 +1,69 @@
+/*
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2018, Telestax Inc and individual contributors
+ * by the @authors tag.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.restcomm.media.plugin.vad;
+
+import java.io.IOException;
+
+import org.restcomm.media.core.resource.vad.VoiceActivityDetector;
+
+/**
+ * Detector for speech signal in provided chunk of audio samples.
+ *
+ * @author Vladimir Morosev (vladimir.morosev@telestax.com)
+ *
+ */
+public class NoiseThresholdDetector implements VoiceActivityDetector {
+
+    private final int silenceLevel;
+
+    public NoiseThresholdDetector(final int silenceLevel) {
+        this.silenceLevel = silenceLevel;
+    }
+
+    /**
+     * Checks the sample buffer for speech signal.
+     *
+     * @param data buffer with samples
+     * @param offset the position of first sample in buffer
+     * @param len the number of samples
+     * @return true if silence detected
+     */
+    @Override
+    public boolean detect(byte[] data, int offset, int len) {
+        int[] correllation = new int[len];
+        for (int i = offset; i < len - 1; i += 2) {
+            correllation[i] = (data[i] & 0xff) | (data[i + 1] << 8);
+        }
+
+        double mean = mean(correllation);
+        return mean > silenceLevel;
+    }
+
+    private double mean(int[] m) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i];
+        }
+        return sum / m.length;
+    }
+
+}
